38387167974f544837e54aec1e2ee3fc9ca1b0b5
ddwwS1 <zain.a.h.k.p@gmail.com>
2025-11-14 17:52:36 +0300

    Add hamburger menu functionality to script.js

--- store-project/UI/script.js
+++ store-project/UI/script.js
@@ -1,1 +1,11 @@
+console.log("script.js is working!");
+alert("Your JavaScript file is linked!");
 
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () =>{
+hamMenu.classList.toggle('active');
+offScreenMenu.classList.toggle('active');
+})
